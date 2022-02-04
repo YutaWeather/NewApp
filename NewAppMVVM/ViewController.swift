d85b6b3a3df3dcc06861e5c1e4030980fb6d710e
Yuta Fujii <yutayutakid21@gmail.com>
2022-02-04 16:23:43 +0900

    Initial Commit

---
+++ NewAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewAppMVVM
+//
+//  Created by Yuta Fujii on 2022/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
